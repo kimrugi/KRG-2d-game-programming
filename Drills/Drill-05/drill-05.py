a403dcf5f044d4b6d6055ed7c3b1aa1d8f4c34c6
kimrugi(Laptop 1) <kimrugi@gmail.com>
2018-09-18 21:35:14 +0900

    문제를 이해하고 고침
    파이썬의 전역변수, 지역변수에 대한 것을 이해하고 문제를 고침

--- Drills/Drill-05/drill-05.py
+++ Drills/Drill-05/drill-05.py
@@ -5,46 +5,54 @@ open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
+speed = 5
 
 
-def animation_character(frame, theta):
-    if theta > 90 or theta < 270 :
+def animation_character(theta):
+    if -90 < theta < 90:
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
     else:
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
-    frame = (frame + 1) % 8
 
 
-def calculate_degree(x, y, to_x, to_y):
+def calculate_degree(to_x, to_y):
     return math.atan2(to_y - y, to_x - x)
 
 
-def move_to(x, y, to_x, to_y, frame):
-    speed = 1
-    theta = calculate_degree(x, y, to_x, to_y)
-    while to_x != x or to_y != y:
+def move_to(to_x, to_y):
+    global x
+    global y
+    global frame
+    theta = calculate_degree(to_x, to_y)
+    degree = math.degrees(theta)
+    sin = math.sin(theta)
+    cos = math.cos(theta)
+    print(degree)
+    print(sin)
+    print(cos)
+    while not (x - 5 < to_x < x + 5) and not (y - 5 < to_y < y + 5):
         clear_canvas()
         grass.draw(400, 30)
-        animation_character(frame, theta)
+        animation_character(theta)
+        frame = (frame + 1) % 8
         update_canvas()
-        x = x + speed * math.sin(theta)
-        y = y + speed * math.cos(theta)
-        delay(0.01)
-
+        x = x + speed * math.cos(theta)
+        y = y + speed * math.sin(theta)
+        delay(0.05)
 
 
 while True:
     frame = 0
-    x, y = 711.0, 349.0
-    move_to(x, y, 203, 535, frame)
-    move_to(x, y, 132, 243, frame)
-    move_to(x, y, 535, 470, frame)
-    move_to(x, y, 477, 203, frame)
-    move_to(x, y, 715, 136, frame)
-    move_to(x, y, 316, 225, frame)
-    move_to(x, y, 510, 92, frame)
-    move_to(x, y, 692, 518, frame)
-    move_to(x, y, 682, 336, frame)
-    move_to(x, y, 712, 349, frame)
+    x, y = 712, 349
+    move_to(203, 535)
+    move_to(132, 243)
+    move_to(535, 470)
+    move_to(477, 203)
+    move_to(715, 136)
+    move_to(316, 225)
+    move_to(510, 92)
+    move_to(692, 518)
+    move_to(682, 336)
+    move_to(712, 349)
 
 close_canvas()
