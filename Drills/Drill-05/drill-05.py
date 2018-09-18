46e3490d01ddecf55c151ae5dadd443177043016
kimrugi(Laptop 1) <kimrugi@gmail.com>
2018-09-18 20:54:59 +0900

    뭔가 이상함
    뭔지 모르겠음

--- Drills/Drill-05/drill-05.py
+++ Drills/Drill-05/drill-05.py
@@ -1,45 +1,50 @@
 from pico2d import *
+import math
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-LEFT = 0
-RIGHT = 1
 
-x, y = 203, 535
-speed = 1
-frame = 0
+def animation_character(frame, theta):
+    if theta > 90 or theta < 270 :
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    frame = (frame + 1) % 8
 
-def animation_character():
-    pass
 
-import math
-def calculate_degree(to_x, to_y):
-    return math.atan((to_y - y) / (to_x - x))
+def calculate_degree(x, y, to_x, to_y):
+    return math.atan2(to_y - y, to_x - x)
 
-def move_to(to_x, to_y):
-    theta = calculate_degree(to_x, to_y)
+
+def move_to(x, y, to_x, to_y, frame):
+    speed = 1
+    theta = calculate_degree(x, y, to_x, to_y)
     while to_x != x or to_y != y:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
+        clear_canvas()
+        grass.draw(400, 30)
+        animation_character(frame, theta)
+        update_canvas()
         x = x + speed * math.sin(theta)
         y = y + speed * math.cos(theta)
-        animation_character()
         delay(0.01)
-    pass
+
+
 
 while True:
-    move_to(203, 535)
-    move_to(132, 243)
-    move_to(535, 470)
-    move_to(477, 203)
-    move_to(715, 136)
-    move_to(316, 225)
-    move_to(510, 92)
-    move_to(692, 518)
-    move_to(682, 336)
-    move_to(712, 349)
+    frame = 0
+    x, y = 711.0, 349.0
+    move_to(x, y, 203, 535, frame)
+    move_to(x, y, 132, 243, frame)
+    move_to(x, y, 535, 470, frame)
+    move_to(x, y, 477, 203, frame)
+    move_to(x, y, 715, 136, frame)
+    move_to(x, y, 316, 225, frame)
+    move_to(x, y, 510, 92, frame)
+    move_to(x, y, 692, 518, frame)
+    move_to(x, y, 682, 336, frame)
+    move_to(x, y, 712, 349, frame)
 
 close_canvas()
