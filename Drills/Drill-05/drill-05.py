6fc12fcbc44376a26eb10e635c63f76c170ca7cc
2d programming <>
2018-09-18 18:37:01 +0900

    move_to()함수 선언
    x, y좌표로 향하는 함수를 선언함

--- Drills/Drill-05/drill-05.py
+++ Drills/Drill-05/drill-05.py
@@ -5,7 +5,19 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def move_to(to_x, to_y):
+    pass
 
-
+while True:
+    move_to(203, 535)
+    move_to(132, 243)
+    move_to(535, 470)
+    move_to(477, 203)
+    move_to(715, 136)
+    move_to(316, 225)
+    move_to(510, 92)
+    move_to(692, 518)
+    move_to(682, 336)
+    move_to(712, 349)
     
 close_canvas()
