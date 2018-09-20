a3e39b6500813360814e1a8eedfc66d529d72a3b
2d programming <>
2018-09-20 18:15:17 +0900

    더 편안하게
    캐릭터가 움직이는 것이 더 편안해 보이게 만듬

--- Drills/Drill-06/move_character_with_key.py
+++ Drills/Drill-06/move_character_with_key.py
@@ -4,17 +4,30 @@ from pico2d import *
 def handle_events():
     global running
     global x
+    global dir
+    global look_dir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                x = x + 10
+                dir = dir + 1
             elif event.key == SDLK_LEFT:
-                x = x - 10
+                dir = dir - 1
             elif event.key == SDLK_ESCAPE:
                 running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir = dir - 1
+            elif event.key == SDLK_LEFT:
+                dir = dir + 1
+    if(dir == -1):
+        look_dir = 0
+    if(dir == 1):
+        look_dir = 1
+    if(dir == 0):
+        look_dir = 2
 
 
 open_canvas()
@@ -24,15 +37,18 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
+look_dir = 1
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * look_dir, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
+    x = x + dir * 5
 
     delay(0.05)
 
