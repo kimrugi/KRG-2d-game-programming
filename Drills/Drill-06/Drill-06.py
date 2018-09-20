eba1e6125ab8b43bd0cc80a30f97bd4b8805ff78
2d programming <>
2018-09-20 19:10:01 +0900

    Drill-06 완성
    2017180016 서진석 Drill-06 완성 커밋횟수 : 9번 https://youtu.be/1faB0GpFrWY

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -12,21 +12,29 @@ speed = 5
 running = 1
 frame = 0
 is_moving = 0
-direction = 0
+direction = 1
+theta = 0
 
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 to_x, to_y = x, y
 hand_x, hand_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 
+
 def calculate_degree():
-    return math.atan2(to_y - y, to_x - x)
+    global theta
+    theta = math.atan2(to_y - y, to_x - x)
+    print(theta)
 
 
-theta = calculate_degree()
+calculate_degree()
 
 
 def cal_direction():
-    pass
+    global direction
+    if -math.pi / 2 < theta < math.pi / 2:
+        direction = 1
+    else:
+        direction = 0
 
 
 def handle_events():
@@ -34,6 +42,7 @@ def handle_events():
     global x, y
     global hand_x, hand_y
     global to_x, to_y
+    global is_moving
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -44,27 +53,19 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
             to_x, to_y = hand_x, hand_y
-            theta = calculate_degree()
+            calculate_degree()
             cal_direction()
-
-    pass
-
-
-def animation_character():
-    if -90 < theta < 90:
-        character.clip_draw(frame * 100, 0, 100, 100, x, y)
-    else:
-        character.clip_draw(frame * 100, 0, 100, 100, x, y)
-
-
-
+            is_moving = 1
 
 
 def move_to():
     global x, y
     global to_x, to_y
+    global is_moving
     x = x + speed * math.cos(theta)
     y = y + speed * math.sin(theta)
+    if (x - 5 < to_x < x + 5) and (y - 5 < to_y < y + 5):
+        is_moving = 0
 
 
 hide_cursor()
@@ -74,10 +75,11 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     hand.draw(hand_x + 25 , hand_y - 26)
+    character.clip_draw(frame * 100, direction * 100, 100, 100, x, y)
     frame = (frame + 1) % 8
     update_canvas()
-    move_to()
-    animation_character()
+    if(is_moving == 1):
+        move_to()
     handle_events()
     delay(0.05)
 
