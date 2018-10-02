cf205ed11ecb5d2de46b683bb62c967b03a470b0
2d programming <>
2018-10-02 18:57:44 +0900

    애니메이션 함수 완성
    ㅈㄱㄴ

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -9,27 +9,28 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 size = 20
 
+
 def move_to(p1, p2):
     pass
 
-def animation_character(theta):
 
+def animation_character(direction):
+    if direction == 0:
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    else:
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
 
-        character.clip_draw(frame * 100, 0, 100, 100, x, y)
-    pass
 
 random_move_list = [(random.randint(100, 1000), random.randint(100, 800)) for n in range(size)]
 roop = 1
+direction = 0
 while True:
     move_to(random_move_list[roop - 1], random_move_list[roop])
     clear_canvas()
     kpu_ground.draw(400, 30)
-    animation_character(theta)
+    animation_character(direction)
     frame = (frame + 1) % 8
     update_canvas()
-    x = x + speed * math.cos(theta)
-    y = y + speed * math.sin(theta)
     delay(0.05)
     roop = (roop + 1) % size
 
