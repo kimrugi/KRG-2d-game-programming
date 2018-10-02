b5abbfbd162795498e74476e6c10c2d8519f0227
2d programming <>
2018-10-02 19:04:53 +0900

    완성한건지...
    모르겠는데 일단 돌아가니

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -9,8 +9,31 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 size = 20
 
+direction = 0
+frame = 0
+x, y = 0, 0
+
 
 def move_to(p1, p2):
+    global x, y
+    global frame
+    global direction
+    x, y = p1
+    if p1[0] - p2[0] > 0:
+        direction = 0
+    else:
+        direction = 1
+    for i in range(1, 100, 10):
+        t = i / 100
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        clear_canvas()
+        kpu_ground.draw(400, 30)
+        animation_character(direction)
+        frame = (frame + 1) % 8
+        update_canvas()
+        delay(0.05)
+    x, y = p2
     pass
 
 
@@ -23,15 +46,9 @@ def animation_character(direction):
 
 random_move_list = [(random.randint(100, 1000), random.randint(100, 800)) for n in range(size)]
 roop = 1
-direction = 0
+
 while True:
     move_to(random_move_list[roop - 1], random_move_list[roop])
-    clear_canvas()
-    kpu_ground.draw(400, 30)
-    animation_character(direction)
-    frame = (frame + 1) % 8
-    update_canvas()
-    delay(0.05)
     roop = (roop + 1) % size
 
 
