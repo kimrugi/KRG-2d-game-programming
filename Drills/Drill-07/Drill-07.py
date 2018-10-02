6463a2d4b7c0301a9c2fba610051c2630eaec8f8
2d programming <>
2018-10-02 18:52:45 +0900

    틀 만듬
    전체적인 틀 만들고 그 함수

---
+++ Drills/Drill-07/Drill-07.py
@@ -0,0 +1,24 @@
+from pico2d import *
+import random
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+size = 20
+
+def move_to(p1, p2):
+    pass
+
+
+random_move_list = [(random.randint(100, 1000), random.randint(100, 800)) for n in range(size)]
+roop = 1
+while True:
+    move_to(random_move_list[roop - 1], random_move_list[roop])
+    roop = (roop + 1) % size
+
+
+
+
