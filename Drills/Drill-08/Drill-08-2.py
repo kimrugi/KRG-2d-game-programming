7af7a2dcbfad9f12867dde24dca7771026a06595
2d programming <>
2018-10-04 18:44:26 +0900

    드로우 올
    만듬

--- Drills/Drill-08/Drill-08-2.py
+++ Drills/Drill-08/Drill-08-2.py
@@ -25,7 +25,20 @@ def animation_character():
         character.clip_draw(frame * 100, 100, 100, 100, x, y)
 
 
+def draw_all():
+    global frame
+    animation_character()
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    animation_character()
+    frame = (frame + 1) % 8
+    update_canvas()
+    delay(0.05)
+    pass
+
+
 def smooth_move(random_list):
+    global x, y
     loop = -1
     while True:
         p1 = random_list[loop]
@@ -36,7 +49,7 @@ def smooth_move(random_list):
             t = i / 100
             x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
             y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-            animation_character()
+
         x, y = p3
         loop = (loop + 1) % size
     pass
