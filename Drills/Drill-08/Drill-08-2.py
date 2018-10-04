9b70ba4b71717826f1cb5006a5b53e0d890197af
2d programming <>
2018-10-04 18:31:28 +0900

    Drill-08-2 시작

---
+++ Drills/Drill-08/Drill-08-2.py
@@ -0,0 +1,55 @@
+from pico2d import *
+import random
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+size = 20
+
+direction = 0
+frame = 0
+x, y = 0, 0
+
+
+def move_to(p1, p2):
+    global x, y
+    global frame
+    global direction
+    x, y = p1
+    if p1[0] - p2[0] > 0:
+        direction = 0
+    else:
+        direction = 1
+    for i in range(1, 100, 10):
+        t = i / 100
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        animation_character(direction)
+        frame = (frame + 1) % 8
+        update_canvas()
+        delay(0.05)
+    x, y = p2
+    pass
+
+
+def animation_character(direction):
+    if direction == 0:
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+
+
+random_move_list = [(random.randint(100, 1000), random.randint(100, 800)) for n in range(size)]
+
+while True:
+    animation_character(direction)
+
+
+
+
+
