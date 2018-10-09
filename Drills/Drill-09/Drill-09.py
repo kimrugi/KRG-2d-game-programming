2f3dc6a516c7e2b88e8144299af7e08a8f7c97e7
2d programming <>
2018-10-09 18:42:46 +0900

    Drill 09 시작

--- Drills/Drill-09/Drill-09.py
+++ Drills/Drill-09/Drill-09.py
@@ -1,14 +1,22 @@
 from pico2d import *
 running = 0
+WIDTH = 800
+HEIGHT = 600
 
 # y = 599
+
+
+
+
 class Boy:
     def __init__(self):
         self.image = load_image('run_animation.png')
         self.x = 0
         self.frame = 0
+    def move(self):
+        self.x = self.x + 5
     def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, 30)
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, 90)
 
 
 class Grass:
@@ -28,15 +36,19 @@ def handle_events():
             running = False
 
 
-open_canvas(1280, 720);
+open_canvas(WIDTH, HEIGHT);
 boy = Boy()
 grass = Grass()
 
-running = 0
+running = 1
 while running:
     handle_events()
+    clear_canvas()
     boy.draw()
     grass.draw()
+    update_canvas()
+
+    boy.move()
     boy.frame = (boy.frame + 1) % 4
 
 close_canvas()
