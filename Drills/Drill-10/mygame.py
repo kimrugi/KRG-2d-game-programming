8996b0874e52b399bac807becfb4ab90bd62cbe5
kimrugi-LapTop <kimrugi@naver.com>
2018-10-11 20:04:20 +0900

    다듬음

--- Drills/Drill-10/mygame.py
+++ Drills/Drill-10/mygame.py
@@ -1,8 +1,9 @@
 import game_framework
 import pico2d
-
+import main_state
 import start_state
 
 pico2d.open_canvas()
-game_framework.run(start_state)
+#game_framework.run(start_state)
+game_framework.run(main_state)
 pico2d.clear_canvas()
