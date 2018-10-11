996b92c09aa29e8207db33d88e77b2e532393ecf
2d programming <>
2018-10-11 18:50:34 +0900

    pause_state 틀

---
+++ Drills/Drill-10/pause_state.py
@@ -0,0 +1,36 @@
+import game_framework
+from pico2d import *
+import title_state
+
+name = ""
+image = None
+
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    pass
+
+
+
+
+def handle_events():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
