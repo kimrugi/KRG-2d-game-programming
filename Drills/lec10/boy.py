e2866113ddfe8144e3c2b4556dd2facc29d52cc9
2d programming <>
2018-10-23 18:27:30 +0900

    흠

--- Drills/lec10/boy.py
+++ Drills/lec10/boy.py
@@ -1,7 +1,7 @@
 from pico2d import *
 
 # Boy Event
-# fill here
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
@@ -14,10 +14,55 @@ key_event_table = {
 
 # Boy States
 
-# fill here
+class IdleState:
+    @staticmethod
+    def enter(boy):
+        boy.frame = 0
+        boy.timer = 1000
+
+    @staticmethod
+    def exit(boy):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 40
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(boy.frame // 5 * 100, 300, 100, 100, boy.x, boy.y)
+        else :
+            boy.image.clip_draw(boy.frame // 5 * 100, 200, 100, 100, boy.x, boy.y)
+
+class RunState:
+    @staticmethod
+    def enter(boy):
+        boy.frame = 0
+        boy.dir = boy.velocity
+
+    @staticmethod
+    def exit(boy):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 40
+        boy.x += boy.velocity
+        boy.x = clamp(25, boy.x, 800 - 25)
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(boy.frame // 5 * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame // 5 * 100, 0, 100, 100, boy.x, boy.y)
 
 next_state_table = {
-# fill here
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
+                RIGHT_DOWN: RunState, LEFT_DOWN: RunState},
+    RunState: {RIGHT_UP:IdleState, LEFT_UP: IdleState,
+               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState}
 }
 
 
@@ -27,37 +72,51 @@ next_state_table = {
 
 
 class Boy:
+    image = None
 
     def __init__(self):
         self.x, self.y = 800 // 2, 90
         self.image = load_image('animation_sheet.png')
         self.dir = 1
         self.velocity = 0
-        # fill here
-        pass
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self)
 
 
     def change_state(self,  state):
-        # fill here
+        self.cur_state.exit(self)
+        self.cur_state = state
+        self.cur_state.enter(self)
         pass
 
 
     def add_event(self, event):
-        # fill here
-        pass
+        self.event_que.insert(0, event)
 
 
     def update(self):
-        # fill here
-        pass
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.change_state(next_state_table[self.cur_state][event])
 
 
     def draw(self):
-        # fill here
+        self.cur_state.draw(self)
         pass
 
 
     def handle_event(self, event):
-        # fill here
-        pass
+        if(event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            if key_event == RIGHT_DOWN:
+                self.velocity += 1
+            elif key_event == LEFT_DOWN:
+                self.velocity -= 1
+            elif key_event == RIGHT_UP:
+                self.velocity -= 1
+            elif key_event == LEFT_UP:
+                self.velocity += 1
+            self.add_event(key_event)
 
--- Drills/lec10/mygame.py
+++ Drills/lec10/mygame.py
@@ -3,6 +3,7 @@ import pico2d
 
 import main_state
 
+
 pico2d.open_canvas()
 game_framework.run(main_state)
 pico2d.close_canvas()
