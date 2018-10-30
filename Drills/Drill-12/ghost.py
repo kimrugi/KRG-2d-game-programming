096505c2ffb94c32b7d340fcb66b387d3031843d
2d programming <>
2018-10-30 18:54:54 +0900

    고스트

--- Drills/Drill-12/ghost.py
+++ Drills/Drill-12/ghost.py
@@ -1,16 +1,17 @@
 
+import game_framework
+from pico2d import *
 
+import game_world
 
 
 
 
 
 class Ghost:
-
-    def __init__(self):
+    image = load_image("animation_sheet.png")
+    def __init__(self, boy):
         pass
-
-
 
 
 
