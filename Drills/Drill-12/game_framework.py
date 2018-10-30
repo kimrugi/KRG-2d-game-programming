5518fc6f8283f9299f3f55ec2ac42a8312693074
2d programming <>
2018-10-30 18:46:23 +0900

    Drill-12 시작

--- Drills/Drill-12/game_framework.py
+++ Drills/Drill-12/game_framework.py
@@ -93,12 +93,13 @@ def run(start_state):
     start_state.enter()
 
     global frame_time
-    # fill here
+    current_time = time.time()
     while (running):
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
-        # fill here
+        frame_time = time.time() - current_time
+        current_time += frame_time
 
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
