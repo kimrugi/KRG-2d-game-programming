9f34f5a1646a1889a376d141114142fe9f28409d
kimrugi-class <kimrugi@gmail.com>
2018-11-20 18:50:14 +0900

    Drill-13 start

--- Drills/lec-15/boy.py
+++ Drills/lec-15/boy.py
@@ -70,12 +70,10 @@ class WalkingState:
         boy.x += boy.x_velocity * game_framework.frame_time
         boy.y += boy.y_velocity * game_framework.frame_time
 
-        # fill here
-
 
     @staticmethod
     def draw(boy):
-        # fill here
+        cx, cy = boy.canvas_width//2, boy.canvas_height // 2
 
         if boy.x_velocity > 0:
             boy.image.clip_draw(int(boy.frame) * 100, 100, 100, 100, cx, cy)
@@ -142,7 +140,7 @@ class Boy:
 
     def draw(self):
         self.cur_state.draw(self)
-        self.font.draw(self.canvas_width//2 - 60, self.canvas_height//2 + 50, '(%5d, %5d)' % (self.x, self.y), (255, 255, 0))
+        self.font.draw(self.canvas_width//2 - 60, self.canvas_height // 2 + 50, '(%5d, %5d)' % (self.x, self.y), (255, 255, 0))
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
