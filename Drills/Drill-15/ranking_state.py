8badc9565cd1b8502a6026bc24467f121059710c
Lee Hyojun <u7i333@naver.com>
2018-11-27 18:44:28 +0900

    ranking_state 틀

--- Drills/Drill-15/ranking_state.py
+++ Drills/Drill-15/ranking_state.py
@@ -1,3 +1,54 @@
+import random
+import json
+import pickle
+import os
 
+from pico2d import *
+import game_framework
+import game_world
+
+import main_state
+
+from boy import Boy
+from zombie import Zombie
+import world_build_state
+
+boy = None
+
+
+name = "WorldBuildState"
+font = load_font('ENCR10B.TTF', 20)
+menu = None
+
+def enter():
+
+
+def exit():
+    global menu
+    del menu
+
+def pause():
+    pass
+
+def resume():
+    pass
+
+def get_boy():
+    return boy
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.change_state(world_build_state)
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    menu.draw(get_canvas_width()//2, get_canvas_height()//2)
+    update_canvas()
 
 
